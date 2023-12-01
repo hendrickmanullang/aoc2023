ee2910b4c1eb475ed542b09834f7ed200e51c6ae
Hendrick Manullang <h.manullang@gmail.com>
2023-12-01 16:56:26 +1100

    minor update

--- day1.js
+++ day1.js
@@ -7,7 +7,6 @@ const lines = data.split("\n");
 let sum = 0;
 
 for (let i = 0; i < lines.length - 1; i++) {
-  console.log(sum);
   const numbers = lines[i].replace(/[^0-9]/g, "");
   if (numbers.length === 2) {
     sum += parseInt(numbers);
@@ -17,3 +16,5 @@ for (let i = 0; i < lines.length - 1; i++) {
     sum += parseInt(numbers[0] + numbers[numbers.length - 1]);
   }
 }
+
+console.log(sum);
